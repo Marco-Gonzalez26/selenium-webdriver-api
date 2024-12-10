8e7eccda0615cf6cdf12f2e5d9d0cd02371cbec6
Marco Gonzalez <magp2606@gmail.com>
2024-12-10 14:59:17 -0500

    Add vercel json

--- src/index.js
+++ src/index.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 
 app.use(express.json())
-app.get('/', (req, res) => res.send('Express on Vercel'))
+app.get('/api', (req, res) => res.send('Express on Vercel'))
 // Routes
-app.use('/webdriver', webdriverRoutes)
+app.use('/api/webdriver', webdriverRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
